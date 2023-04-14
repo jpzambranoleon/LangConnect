cbb7e21d7a2564b6b831256c3af0ad774775c614
Jean-Paul <jpzambrano.leon@gmail.com>
2023-04-14 15:12:40 -0500

    created files

---
+++ models/user.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+    default: "/broken-image.jpg",
+  },
+  nativeLanguage: {
+    type: String,
+    required: true,
+  },
+  learningLanguage: {
+    type: String,
+    required: true,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
