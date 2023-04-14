771c6e381a8448f0b0d87e9f3d4642f58092d61c
Jean-Paul <jpzambrano.leon@gmail.com>
2023-04-14 15:31:01 -0500

    modified code

--- models/user.model.js
+++ models/user.model.js
@@ -46,6 +46,10 @@ const userSchema = new mongoose.Schema({
       ref: "Post",
     },
   ],
+  verified: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
