f8ee853b6c1fc1d28fcd44bd8104a762f81ff107
Jean-Paul <jpzambrano.leon@gmail.com>
2023-04-15 01:45:44 -0500

    modified file

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -58,7 +58,6 @@ export default function Login() {
               label="Email Address"
               name="email"
               autoComplete="email"
-              autoFocus
             />
             <TextField
               margin="normal"
